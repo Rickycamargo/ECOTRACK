2b023da36eb9c8127a59c14c48264776ba2df5b6
Rickycamargo <ryanr7018@gmail.com>
2024-10-01 17:58:23 -0300

    Add files via upload

---
+++ Login.js
@@ -0,0 +1,28 @@
+var btnSignin = document.querySelector("#signin");
+var btnSignup = document.querySelector("#signup");
+
+var btnSubmitSignin = document.querySelector("#submitSignin"); 
+var btnSubmitSignup = document.querySelector("#submitSignup"); 
+
+var body = document.querySelector("body");
+
+
+btnSignin.addEventListener("click", function () {
+    body.className = "sign-in-js";
+});
+
+
+btnSignup.addEventListener("click", function () {
+    body.className = "sign-up-js";
+});
+
+
+btnSubmitSignin.addEventListener("click", function (e) {
+    e.preventDefault(); 
+    window.location.href = "formulario.html"; 
+});
+
+btnSubmitSignup.addEventListener("click", function (e) {
+    e.preventDefault(); 
+    window.location.href = "link-para-cadastro.html";
+});
